7ff85bd9390c246b232d33932ec0692352a76c8d
Rodrigo Rech <rodrigorech@hotmail.com>
2021-09-26 23:36:39 -0300

    add métodos p/ gerenciar filas
    add métodos p/ gerenciar filas de mensagens, como add na fila, remover da fila, pegar primeiro da fila...

--- fila.c
+++ fila.c
@@ -1,5 +1,7 @@
+#include <pthread.h>
 #include <stdbool.h>
 #include <arpa/inet.h>
+#define QTD_MENSAGENS 3
 
 /*
     A estrutura da mensagem a ser trocada pelos roteadores. Pode ser uma mensagem de controle ou mensagem de dado
@@ -16,7 +18,36 @@ typedef struct {
 } mensagem;
 
 typedef struct {
-    pthread_mutex_t mutex;
-    mensagem mensagens[10];
+    mensagem mensagens[QTD_MENSAGENS];
 } fila_de_saida;
 
+pthread_mutex_t mutex_fila_saida = PTHREAD_MUTEX_INITIALIZER;
+int tamanho_atual_fila_saida = 0;
+
+/*Add elemento no final da fila*/
+void fila_saida_add(mensagem mensagem_nova, fila_de_saida fila_saida) {
+    pthread_mutex_lock(&mutex_fila_saida);
+    tamanho_atual_fila_saida++;
+    fila_saida.mensagens[tamanho_atual_fila_saida] = mensagem_nova;
+    pthread_mutex_unlock(&mutex_fila_saida);
+}
+
+/*Remove elemento do inicio da fila*/
+void fila_saida_remove(fila_de_saida fila_saida) {
+    pthread_mutex_lock(&mutex_fila_saida);
+    for(int i = 0; i < tamanho_atual_fila_saida; i++) {
+        fila_saida.mensagens[i] = fila_saida.mensagens[i+1];
+    }
+    tamanho_atual_fila_saida--;
+    pthread_mutex_unlock(&mutex_fila_saida);
+}
+
+mensagem fila_saida_get(fila_de_saida fila_saida) {
+    pthread_mutex_lock(&mutex_fila_saida);
+    return fila_saida.mensagens[0];
+    pthread_mutex_unlock(&mutex_fila_saida);
+}
+
+bool existe_elemento_fila_saida() {
+    return (tamanho_atual_fila_saida == 0) ? false : true;
+}
